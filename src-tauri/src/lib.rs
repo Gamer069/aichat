ac15268b70e59e03cec48e9d883e4baa2f176f59
Anton Zhdanov <anton.s.zhdanov@gmail.com>
2025-06-02 07:36:57 +0300

    initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,43 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+pub enum Role {
+    User,
+    Ai
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Role::User => {
+                f.write_str("User")
+            },
+            Role::Ai => {
+                f.write_str("Ai")
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Message {
+    pub role: Role,
+    pub content: String,
+}
+
+#[tauri::command]
+fn process_message(message: Message) -> Message {
+    Message { role: Role::Ai, content: "something".to_owned() }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![process_message])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/app.rs
@@ -0,0 +1,103 @@
+use std::fmt::Display;
+
+use leptos::{ev::SubmitEvent, leptos_dom::logging::console_log, prelude::*, task::spawn_local};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(what: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub enum Role {
+    User,
+    Ai
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Role::User => {
+                f.write_str("User")
+            },
+            Role::Ai => {
+                f.write_str("Ai")
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Message {
+    pub role: Role,
+    pub content: String,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (user_messages, set_user_messages) = signal(vec![
+        Message { role: Role::User, content: "wqewerew".to_string() }
+    ]);
+
+    view! {
+        <div class="messages">
+            <For
+                each=move || user_messages.get()
+                key=|msg| format!("{}{}", msg.role, msg.content)
+                children=move |msg| {
+                    view! {
+                        <div class="userMessages message">
+                            { msg.content }
+                        </div>
+                    }
+                }
+            />
+            <div class="aiMessages message">
+                "Lorem ipsum dolor sit amet consectetur edipiscing elit!"
+            </div>
+        </div>
+
+        <main class="container">
+            <form class="row" on:submit=move |ev: SubmitEvent| {
+                ev.prevent_default();
+
+                let inp = ev.target()
+                    .and_then(|target| target.dyn_into::<leptos::web_sys::HtmlFormElement>().ok())
+                    .and_then(|form| form.elements().named_item("message"))
+                    .and_then(|elem| elem.dyn_into::<leptos::web_sys::HtmlInputElement>().ok());
+
+                if let Some(inp) = inp {
+                    if !inp.value().trim().is_empty() {
+                        let msg = Message {
+                            role: Role::User, content: inp.value().trim().to_string() 
+                        };
+
+                        set_user_messages.update(|messages| {
+                            messages.push(msg.clone());
+                        });
+
+                        spawn_local(async move {
+                            let args = serde_wasm_bindgen::to_value(&serde_json::json!({ "message": msg })).unwrap();
+                            let response = invoke("process_message", args).await;
+                            let response: Message = serde_wasm_bindgen::from_value(response).unwrap();
+                            set_user_messages.update(|messages| {
+                                messages.push(response);
+                            });
+                            console_log("Message added!");
+                        });
+                    }
+
+                    inp.set_value("");
+                }
+            }>
+                <input
+                    id="message"
+                    placeholder="Enter a message..."
+                />
+                <button type="submit">"Send"</button>
+            </form>
+        </main>
+    }
+}
